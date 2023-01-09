8b00256b3194cdb2d736d0ec966080b6b16e41f4
Ashim54321 <81402762+Ashim54321@users.noreply.github.com>
2023-01-09 04:12:48 -0800

    dfcgvbhnm

--- Auth/src/main/java/com/authentication/lms/LmsApplication.java
+++ Auth/src/main/java/com/authentication/lms/LmsApplication.java
@@ -5,14 +5,21 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @SpringBootApplication
+@RestController
 public class LmsApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LmsApplication.class, args);
+	}
+	@GetMapping("/")
+	public String intro(){
+		return "Hello there";
 	}
 
 }
